e2ae251fd7fd6f0c77fd8773dbb902a93a1202a4
Keegan Lawley <keeganlk.7@gmail.com>
2021-10-26 00:11:19 -0500

    my changes after forking

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -41,18 +41,6 @@ export const Footer: React.FC<{
       ) : null}
 
       <div className={styles.social}>
-        {config.twitter && (
-          <a
-            className={styles.twitter}
-            href={`https://twitter.com/${config.twitter}`}
-            title={`Twitter @${config.twitter}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitter />
-          </a>
-        )}
-
         {config.github && (
           <a
             className={styles.github}
